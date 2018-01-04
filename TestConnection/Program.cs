288170e55aab78754c2de2b1653d6c5fa2586a0d
ResFieldpro <resfieldpro@gmail.com>
2018-01-04 18:56:55 +0300

    convert data to datatable

--- TestConnection/Program.cs
+++ TestConnection/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,6 +31,29 @@ namespace WebDataLoaderTest
 
 			Console.WriteLine("Session ID:");
 			Console.WriteLine(wi.GetSession(web, usr, pas));
+
+			Console.WriteLine("Output name:");
+			string output = Console.ReadLine();
+			if (string.IsNullOrEmpty(output))
+				output = "PetrelSource";
+
+			Console.WriteLine("Output:");
+			DataTable dt = wi.GetOutput(web, usr, pas, output);
+			foreach (DataColumn dc in dt.Columns)
+			{
+				Console.Write(dc.ColumnName + "\t");
+			}
+			Console.WriteLine("");
+
+			foreach (DataRow dr in dt.Rows)
+			{
+				int cols = dt.Columns.Count;
+				for (int i = 0; i < cols; i++)
+				{
+					Console.WriteLine(dr[i] as string);
+				}
+			}
+			
 			Console.ReadKey();
 		}
 	}
--- WebDataLoader.cs
+++ WebDataLoader.cs
@@ -2,10 +2,14 @@
 using System.CodeDom;
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Reflection;
 using System.Security.Principal;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web.Services.Description;
 
@@ -13,12 +17,18 @@ namespace WebDataLoader
 {
 	public class WebImporter
 	{
+		static int NameSpaceCounter = 0;
 		static string ObjectManagerServiceName = "ObjectManagerService";
 		static Dictionary<string, object> WebServices = new Dictionary<string, object>();
 		static Dictionary<string, string> WebSessions = new Dictionary<string, string>();
 		string Webhash(string WebServer, string WebServerU, string WebServerP)
 		{
-			return "server:" + WebServer + "user:" + WebServerU + "password:" + WebServerP;
+			return $"server:{WebServer}user:{WebServerU}password:{WebServerP}";
+		}
+		void CorrectWebAddress(ref string WebServer)
+		{
+			if (WebServer.Substring(WebServer.Length - 1) != "/")
+				WebServer += "/";
 		}
 		void GetCorrectedUserCreds(ref string WebServerU, ref string WebServerP)
 		{
@@ -30,12 +40,16 @@ namespace WebDataLoader
 				s.GetBinaryForm(binaryForm, 0);
 				string res = "";
 				foreach (byte b in binaryForm)
+				{
 					res += b.ToString("x2");
+				}
 				WebServerU = "WINLGN:" + res;
 			}
 		}
 		object GetObjectManagerService(string WebServer, string WebServerU, string WebServerP)
 		{
+			if (string.IsNullOrEmpty(WebServer))
+				throw new Exception("Invalid web server address!");
 			//store all instances of service class as each instance is a separate dll and loaded each time into memory
 			string hash = Webhash(WebServer, WebServerU, WebServerP);
 			if (WebServices.ContainsKey(hash))
@@ -46,15 +60,18 @@ namespace WebDataLoader
 			ServiceDescriptionImporter importer = new ServiceDescriptionImporter();
 			using (System.Net.WebClient client = new System.Net.WebClient())
 			{
+				CorrectWebAddress(ref WebServer);
+
 				System.IO.Stream stream = client.OpenRead(WebServer + ObjectManagerServiceName + ".asmx?wsdl");
-				ServiceDescription description = ServiceDescription.Read(stream);
 				importer.ProtocolName = "Soap12";
+				ServiceDescription description = ServiceDescription.Read(stream);
 				importer.AddServiceDescription(description, null, null);
 				importer.Style = ServiceDescriptionImportStyle.Client;
 			}
 
 			importer.CodeGenerationOptions = System.Xml.Serialization.CodeGenerationOptions.GenerateProperties;
 			CodeNamespace nmspace = new CodeNamespace();
+			NameSpaceCounter++;
 			CodeCompileUnit CompileUnit = new CodeCompileUnit();
 			CompileUnit.Namespaces.Add(nmspace);
 			ServiceDescriptionImportWarnings warning = importer.Import(nmspace, CompileUnit);
@@ -72,7 +89,7 @@ namespace WebDataLoader
 				WebServices[hash] = wsvcClass;
 				return wsvcClass;
 			}
-			throw new Exception("Failed to connect to web servie. Check web server exists.");
+			throw new Exception(message: "Failed to connect to web servie. Check web server exists.");
 		}
 		public string GetSession(string WebServer, string WebServerU, string WebServerP)
 		{
@@ -89,5 +106,110 @@ namespace WebDataLoader
 			WebSessions[hash] = websession;
 			return websession;
 		}
+		public DataTable GetOutput(string WebServer, string WebServerU, string WebServerP, string outputname)
+		{
+			string session = GetSession(WebServer, WebServerU, WebServerP);
+			object ObjectManagerService = GetObjectManagerService(WebServer, WebServerU, WebServerP);
+			MethodInfo mi = ObjectManagerService.GetType().GetMethod("GetGlobalReportID");
+			object[] argss = new object[2] { session, outputname};
+			string id = mi.Invoke(ObjectManagerService, argss).ToString();
+
+			CorrectWebAddress(ref WebServer);
+			string url = $"{WebServer}viewoutput.aspx?OutputID={id}&VT=16&Context=%3a450+{id}&SkipWait=1&ServiceSessionID={session}";
+
+			Console.WriteLine(url);
+			Uri ur = new Uri(url);
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(ur);
+			request.Timeout = 600000;
+			HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+			string res = "";
+			if (response.StatusCode == HttpStatusCode.OK)
+			{
+				Stream receiveStream = response.GetResponseStream();
+				StreamReader readStream = null;
+
+				if (response.CharacterSet == null)
+				{
+					readStream = new StreamReader(receiveStream);
+				}
+				else
+				{
+					readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+				}
+
+				res = readStream.ReadToEnd();
+				response.Close();
+				readStream.Close();
+			}
+			return BuildDataSet(res);
+		}
+		private DataTable BuildDataSet(string str)
+		{
+			DataTable dt = new DataTable();
+			if (string.IsNullOrEmpty(str))
+				return dt;
+
+			str = Regex.Replace(str, @"^\s+$[\r\n]*", "", RegexOptions.Multiline);
+
+			int counter = 0;
+			bool[] istext = new bool[100];
+			using (StringReader reader = new StringReader(str))
+			{
+				string sline;
+				while ((sline = reader.ReadLine()) != null)
+				{
+					if (string.IsNullOrEmpty(sline))
+						continue;
+
+					string[] arr = sline.Split('\t');
+					if (counter == 0)
+					{
+						foreach (string col in arr)
+						{
+							DataColumn dc = new DataColumn(col.Replace(".", ""), Type.GetType("System.String"));
+							dt.Columns.Add(dc);
+						}
+					}
+					else
+					{
+						DataRow dr = dt.NewRow();
+						for (int i = 0; i < arr.Length; i++)
+						{
+							dr[i] = arr[i];
+							double res;
+							if (istext[i] != true && !(string.IsNullOrEmpty(arr[i])) && !double.TryParse(arr[i], out res))
+							{
+								istext[i] = true;
+							}
+						}
+						dt.Rows.Add(dr);
+					}
+					counter++;
+				}
+			}
+			DataTable dtCloned = dt.Clone();
+			for (int i = 0; i < dtCloned.Columns.Count; i++)
+			{
+				dtCloned.Columns[i].DataType = istext[i] ? typeof(string) : typeof(double);
+			}
+			foreach (DataRow row in dt.Rows)
+			{
+				DataRow dr = dtCloned.NewRow();
+				for (int i = 0; i < dtCloned.Columns.Count; i++)
+				{
+					if (dtCloned.Columns[i].DataType == typeof(double))
+					{
+						if (!string.IsNullOrEmpty(row[i].ToString()))
+							dr[i] = Convert.ToDouble(row[i]);
+					}
+					else
+						dr[i] = row[i];
+				}
+				dtCloned.Rows.Add(dr);
+			}
+			return dtCloned;
+		}
+
 	}
 }
